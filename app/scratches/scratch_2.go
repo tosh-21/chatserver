1670d2a37a8eda61a3ddc52f0db60e6c0eab98cb
Santhosh Mahadevan <tosh21@gmail.com>
2022-03-19 12:57:19 -0700

    Added Scratch File

---
+++ app/scratches/scratch_2.go
@@ -0,0 +1,55 @@
+package main
+
+import "C"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+type User struct {
+	UserName   string
+	ScreenName string
+	Password   string
+}
+type ChatServer struct {
+	UserConnections []net.Conn
+	Users           map[string]User
+}
+
+func main() {
+	chatServer1 := ChatServer{}
+	chatServer1.Users = make(map[string]User)
+
+	chatServer1.Users["test"] = GetUserData()
+
+	fmt.Println(chatServer1.Users)
+}
+
+func PromptQuestion(question string) string {
+	fmt.Printf("%s", question)
+	reader := bufio.NewReader(os.Stdin)   //initialize reader
+	input, err := reader.ReadString('\n') //user enters messsage
+	if err != nil {
+		log.Fatal("Error while reading input")
+	}
+	return strings.TrimSpace(input)
+}
+
+func GetUserData() User {
+	UN := PromptQuestion("Please enter your Username: ")
+	PW := PromptQuestion("Please enter your Password: ")
+	SN := PromptQuestion("Please enter your Screen Name: ")
+
+	UserData := User{
+		UserName:   UN,
+		ScreenName: SN,
+		Password:   PW,
+	}
+
+	return UserData
+
+}
